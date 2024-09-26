58303c9c4784e78a73071c9264ad21035c2e43fd
DIlshad Akthar <dilshad@noviindus.com>
2024-09-26 07:17:35 +0530

    chnages

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -315,38 +315,7 @@ class _State extends State<LoginPage> {
                     ),
                   ],
                 ),
-                Padding(
-                    padding:
-                        const EdgeInsets.only(right: 10, left: 10, top: 20),
-                    child: SizedBox(
-                        width: double.infinity,
-                        // height: 50,
-                        child: ElevatedButton(
-                          
-                          onPressed: () {
-                            Navigator.pushAndRemoveUntil(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => category()),
-                                (route) => false);
-                          },
-                          style: ElevatedButton.styleFrom(
-                            
-                            backgroundColor: const Color(0xff5473bb),
-                            //  elevation: 10,
-                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
-                            //shape: const StadiumBorder(),
-                            padding: const EdgeInsets.symmetric(
-                              vertical: 15,
-                              horizontal: 50,
-                            ),
-                          ),
-                          //
-                          child: const Text(
-                            "CONTINUE",
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        ))),
+                const CustomButton(name: 'CONTINUE',),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
@@ -367,6 +336,46 @@ class _State extends State<LoginPage> {
                   ],
                 )
               ],
+            )));
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final String name;
+  final VoidCallback? onTap;
+  const CustomButton({
+    super.key,
+    required this.name,
+    this.onTap
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding:
+            const EdgeInsets.only(right: 10, left: 10, top: 20),
+        child: SizedBox(
+            width: double.infinity,
+            // height: 50,
+            child: ElevatedButton(
+              
+              onPressed:onTap,
+              style: ElevatedButton.styleFrom(
+                
+                backgroundColor: const Color(0xff5473bb),
+                //  elevation: 10,
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+                //shape: const StadiumBorder(),
+                padding: const EdgeInsets.symmetric(
+                  vertical: 15,
+                  horizontal: 50,
+                ),
+              ),
+              //
+              child:  Text(
+                name,
+                style: const TextStyle(color: Colors.white),
+              ),
             )));
   }
 }
